33dd27242b83c8aa389f0a51c172693cb6ad8bb7
Yannick-nguessan <110746773+Yannick-nguessan@users.noreply.github.com>
2022-10-04 22:18:42 +0200

    Update index.js
    Signed-off-by: Yannick-nguessan <110746773+Yannick-nguessan@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 
 
-var b=Math.random();
-var number1=Math.floor(b*6)+1;
-var number2=Math.floor(Math.random()*6)+1;
 
-var randomimgsrc1 = "images/dice" + number1 + ".png";
-var randomimgsrc2 = "images/dice" + number2 + ".png";
+$("button").click(function(){
+ 
+    launch();
+  
+  
+});
 
+function launch(){
+  
+ $("img").fadeOut(400).fadeIn();
+ var b=Math.random();
+ var number1=Math.floor(b*6)+1;
+ var number2=Math.floor(Math.random()*6)+1;
+ 
+ var randomimgsrc1 = "images/dice" + number1 + ".png";
+ var randomimgsrc2 = "images/dice" + number2 + ".png";
+ 
+ 
+ var image1 =document.querySelectorAll("img")[0];
+ var image2 =document.querySelectorAll("img")[1];
+ 
 
-var image1 =document.querySelectorAll("img")[0];
-var image2 =document.querySelectorAll("img")[1];
-
-image1.setAttribute("src",randomimgsrc1);
-image2.setAttribute("src",randomimgsrc2);
-
-
+    image1.setAttribute("src",randomimgsrc1);
+    image2.setAttribute("src",randomimgsrc2);
     
-if (number1 > number2){
-    document.querySelector("h1").innerHTML=" 🚩Player 1 WIN" ;
+    
+        
+    if (number1 > number2){
+        document.querySelector("h1").innerHTML=" 🚩Player 1 WIN" ;
+    }
+    else if (number1===number2){
+        document.querySelector("h1").innerHTML=" Draw !";
+    }
+    else{
+        document.querySelector("h1").innerHTML=" Player 2 WIN 🚩" ;
+    }
+    
+    setTimeout (function(){
+        $("h1").slideDown().fadeOut(1).fadeIn(100);
+        $("h1").text("Press Launch to start the game")
+        $("img").attr("src","images/dice6.png");
+      
+       },3500);
 }
-else if (number1===number2){
-    document.querySelector("h1").innerHTML=" Draw !";
-}
-else{
-    document.querySelector("h1").innerHTML=" Player 2 WIN 🚩" ;
-}
+
